ba0b26c2bc07964fa6312f7b439b8e9d8fdf7ee0
James Park <james.park.victoria@gmail.com>
2021-09-10 15:44:54 -0700

    Initial routing and add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import "./App.css";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const list = [1, 2, 3, 4];
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+import Home from "./pages/Home/Home";
+
+const navList = ["About", "News", "Contact Us"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          We are a UVic Student club dedicated to studying the <b>Bible.</b>
-        </p>
-        {list.map((number) => (
-          <div>{number}</div>
-        ))}
-      </header>
       <Router>
-        {list.map((number) => (
-          <div>number</div>
-        ))}
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Nav>
+                <Navbar.Brand href="/">
+                  <img
+                    src={require("./ubf_logo_white.png").default}
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="UBF logo"
+                  />
+                </Navbar.Brand>
+
+                {navList.map((option) => (
+                  <Nav.Link href={`/${option}`}>{option}</Nav.Link>
+                ))}
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+
+        <body>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/:pageId" component={Page} />
+          </Switch>
+        </body>
       </Router>
     </div>
   );
 }
+
+const Page = ({ match }) => {
+  return <div>{match.params.pageId}</div>;
+};
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      We are a UVic student club dedicated to studying the <b>Bible</b>
+    </div>
+  );
+};
+
+export default Home;
