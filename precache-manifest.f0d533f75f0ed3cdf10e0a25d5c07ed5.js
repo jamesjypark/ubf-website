b6282bb587f9272386e94e569cc9b8eae2ba69bd
James Park <james.park.victoria@gmail.com>
2022-01-18 23:13:40 -0800

    Updates

---
+++ precache-manifest.f0d533f75f0ed3cdf10e0a25d5c07ed5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed477ca26e7298b1baf",
+    "url": "/static/css/main.f54484ba.chunk.css"
+  },
+  {
+    "revision": "fed477ca26e7298b1baf",
+    "url": "/static/js/main.fed477ca.chunk.js"
+  },
+  {
+    "revision": "fc3557f4d91b9107f886",
+    "url": "/static/js/1.fc3557f4.chunk.js"
+  },
+  {
+    "revision": "0a83c270cf672b2c42ba",
+    "url": "/static/css/2.e2446ad5.chunk.css"
+  },
+  {
+    "revision": "0a83c270cf672b2c42ba",
+    "url": "/static/js/2.0a83c270.chunk.js"
+  },
+  {
+    "revision": "a2ee385787d7fccccbed",
+    "url": "/static/js/runtime~main.a2ee3857.js"
+  },
+  {
+    "revision": "15b230b7007d7d6bf7a9bbb64a10510a",
+    "url": "/static/media/bible.15b230b7.png"
+  },
+  {
+    "revision": "3a8d20eab20ca4e9b83c5571bfdbb6e7",
+    "url": "/static/media/share.3a8d20ea.png"
+  },
+  {
+    "revision": "868f855fd24aa12db0f47121a3835c79",
+    "url": "/static/media/worship.868f855f.png"
+  },
+  {
+    "revision": "18adddff8673cdb77d5b61e5de5fc061",
+    "url": "/static/media/down-arrow.18adddff.png"
+  },
+  {
+    "revision": "7ded32fa0941ade590c0179f23d84fa1",
+    "url": "/static/media/ubf_logo.7ded32fa.png"
+  },
+  {
+    "revision": "60d3d273c86da1d622c23f3871f81dfb",
+    "url": "/static/media/ubf_logo_white.60d3d273.png"
+  },
+  {
+    "revision": "5b320b112a36b459c81ced4f0227eceb",
+    "url": "/static/media/promo.5b320b11.jpg"
+  },
+  {
+    "revision": "cd5aa4c67a3cb266e561e6dc6f4bf8a1",
+    "url": "/index.html"
+  }
+];
